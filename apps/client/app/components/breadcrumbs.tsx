3c25f610d34323665beda905264bc1b89bfddb65
Arthur Green <curious.dev@icloud.com>
2025-01-06 13:07:56 +0400

    fix: only 9 typing errors left (#437)
    * fix: first typing
    
    * fix: only 10 typing errors left
    
    * fix: till Found 9 errors.
    
    * chore: remove unused line

--- apps/client/app/components/breadcrumbs.tsx
+++ apps/client/app/components/breadcrumbs.tsx
@@ -2,14 +2,20 @@ import { useMatches } from 'react-router';
 
 import { breadcrumbs, crumb as StCrumb } from './breadcrumbs.css';
 
+interface RouteHandle {
+  crumb: (data: unknown) => React.ReactNode;
+}
+
 function Breadcrumbs() {
   const matches = useMatches();
   const crumbs = matches
     // first get rid of any matches that don't have handle and crumb
-    .filter((match) => Boolean(match.handle?.crumb))
+    .filter((match) =>
+      Boolean((match.handle as RouteHandle | undefined)?.crumb),
+    )
     // now map them into an array of elements, passing the loader
     // data to each one
-    .map((match) => match.handle.crumb(match.data));
+    .map((match) => (match.handle as RouteHandle).crumb(match.data));
 
   return (
     <div className="container mx-auto px-4">
