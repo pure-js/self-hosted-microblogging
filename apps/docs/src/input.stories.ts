f4f50f9aed6a1b5c9da0a0b1f46ca11accfda2e5
Arthur Green <curious.dev@icloud.com>
2025-03-02 21:49:26 +0400

    build: reinit storybook setup (#469)
    * build: re init storybook setup
    
    * ci: fix formatter workflow
    
    * test: add storybook/experimental-addon-test/vitest-plugin

--- apps/docs/src/input.stories.ts
+++ apps/docs/src/input.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Input } from './input';
+import { Input } from 'client/app/components/input';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta = {
