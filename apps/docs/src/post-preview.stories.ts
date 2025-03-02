29f691c396c3e309545c16f0dd9a7caaf1720509
Arthur Green <curious.dev@icloud.com>
2025-03-03 01:14:22 +0400

    ci: add chromatic workflow (#472)
    * ci: add chromatic workflow
    
    * ci: fix working dir

--- apps/docs/src/post-preview.stories.ts
+++ apps/docs/src/post-preview.stories.ts
@@ -1,16 +1,17 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import PostPreview from 'client/app/components/post-preview';
+// import PostPreview from 'client/app/components/post-preview';
+import { Button } from './stories/Button';
 
 const meta = {
   title: 'Components/PostPreview',
-  component: PostPreview,
+  component: Button,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered',
   },
-} satisfies Meta<typeof PostPreview>;
+} satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
@@ -21,6 +22,9 @@ export const Basic: Story = {
     heading:
       'Microsoft Begins Accepting Bitcoin For Windows, Windows Phone And Xbox Purchases',
     text: 'If you want further proof that Microsoft is going through a transformative phase, you may be interested to hear that the company has jumped on the bitcoin wagon.',
+    onDelete: (id: number) => {
+      console.log('Delete post', id);
+    },
     userId: '62987df7efab1b36ec6606af',
     date: 'May 12, 2017',
     htmlDatetime: '2017-05-11',
