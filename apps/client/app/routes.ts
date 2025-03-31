4665b474c51f242406165db470fefa266971962b
Arthur Green <curious.dev@icloud.com>
2025-03-31 23:03:04 +0400

    chore: remove unused code

--- apps/client/app/routes.ts
+++ apps/client/app/routes.ts
@@ -1,9 +1,5 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-// import { type RouteConfig, index } from '@react-router/dev/routes';
 import { type RouteConfig } from '@react-router/dev/routes';
 import { flatRoutes } from '@react-router/fs-routes';
 
-export default [
-  // index('routes/index.tsx'),
-  ...(await flatRoutes()),
-] satisfies RouteConfig;
+export default [...(await flatRoutes())] satisfies RouteConfig;
