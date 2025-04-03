7dfa4bde63ffdbe01c3bdb7acae2db30acb4a7c0
Arthur Green <curious.dev@icloud.com>
2025-04-03 19:24:13 +0400

    test: make test even more accessible

--- tests/new-post.e2e.spec.ts
+++ tests/new-post.e2e.spec.ts
@@ -5,9 +5,13 @@ test.beforeEach(async ({ page }) => {
 });
 
 test('should create a new post with a title and body', async ({ page }) => {
-  await page.getByRole('textbox', { name: 'Title' }).fill('Test Heading');
-  await page.getByRole('textbox', { name: 'The message' }).fill('Test body');
+  await page.getByRole('textbox', { name: 'Title' }).fill('New Post Heading');
+  await page
+    .getByRole('textbox', { name: 'The message' })
+    .fill('New post main text');
   await page.getByRole('button', { name: 'Post it' }).click();
-  await expect(page.getByText('Test Heading')).toBeVisible();
-  await expect(page.getByText('Test body')).toBeVisible();
+  await expect(
+    page.getByRole('heading', { name: 'New Post Heading' }),
+  ).toBeVisible();
+  await expect(page.getByText('New post main text')).toBeVisible();
 });
