9802fbffca45e8028e2f1f3b672e6bef7c8ff834
Arthur Green <curious.dev@icloud.com>
2025-04-03 19:00:05 +0400

    fix: check test check

--- tests/new-post.e2e.spec.ts
+++ tests/new-post.e2e.spec.ts
@@ -1,11 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('./posts/new');
-  await page.getByRole('textbox', { name: 'Title' }).click();
+});
+
+test('should create a new post with a title and body', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Title' }).fill('Test Heading');
-  await page.getByRole('textbox', { name: 'Title' }).press('Tab');
   await page.getByRole('textbox', { name: 'The message' }).fill('Test body');
   await page.getByRole('button', { name: 'Post it' }).click();
-  await expect(page).toHaveURL('./');
+  await expect(page.getByText('Test Heading')).toBeVisible();
+  await expect(page.getByText('Test body')).toBeVisible();
 });
